ddb4b4804d349899e7a95687b3044dbf578e6972
swg27 <sistemaswebg27@gmail.com>
2017-10-18 17:18:38 +0200

    Opcional hecho

--- php/visualizardatos.php
+++ php/visualizardatos.php
@@ -27,20 +27,24 @@ echo '<table border=1>
         <th>Respuesta incorrecta 2</th> 
         <th>Respuesta incorrecta 3</th>
         <th>email</th>
+        <th>Imagen relacionada</th>
         </tr>';
 
 while($row = mysqli_fetch_array($preguntas)){
     echo
     '<tr>
-    <td>'.$row['CodPregunta'].'</td>
-    <td>'.$row['dificultad'].'</td>
-    <td>'.$row['tema'].'</td>
-    <td>'.$row['pregunta'].'</td>
-    <td>'.$row['respuesta'].'</td>
-    <td>'.$row['no_respuesta_1'].'</td>
-    <td>'.$row['no_respuesta_2'].'</td>
-    <td>'.$row['no_respuesta_3'].'</td>
-    <td>'.$row['email'].'</td>
+    <td align="center">'.$row['CodPregunta'].'</td>
+    <td align="center">'.$row['dificultad'].'</td>
+    <td align="center">'.$row['tema'].'</td>
+    <td align="center">'.$row['pregunta'].'</td>
+    <td align="center">'.$row['respuesta'].'</td>
+    <td align="center">'.$row['no_respuesta_1'].'</td>
+    <td align="center">'.$row['no_respuesta_2'].'</td>
+    <td align="center">'.$row['no_respuesta_3'].'</td>
+    <td align="center">'.$row['email'].'</td>
+    <td align="center">'
+    .'<img src="data:image/type;base64,'.base64_encode( $row['image']).'"/>'.
+    '</td>
     </tr>';
 }
     echo '</table>';
